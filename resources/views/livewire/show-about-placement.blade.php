bb52a6a546e5ea3f57a225c2b2053fc250aca095
darshan45672 <drshnbhandary@gmail.com>
2024-03-05 08:02:12 +0530

    added teachers section

--- resources/views/livewire/show-about-placement.blade.php
+++ resources/views/livewire/show-about-placement.blade.php
@@ -1,6 +1,7 @@
 <div>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-    <div class="site-section ftco-subscribe-1 site-blocks-cover pb-4" style="background-image: url('{{ asset('front/images/bg_1.jpg') }}')">
+    <div class="site-section ftco-subscribe-1 site-blocks-cover pb-4"
+        style="background-image: url('{{ asset('front/images/bg_1.jpg') }}')">
         <div class="mt-5 container">
             <div class="mt-5 row align-items-end">
                 <div class="mt-5 col-lg-7">
@@ -95,6 +96,82 @@
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis recusandae, iure repellat
                         quis delectus ea?
                         Dolore, amet reprehenderit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="site-section">
+        <div class="container">
+            <div class="row mb-5 justify-content-center text-center">
+                <div class="col-lg-4 mb-5">
+                    <h2 class="section-title-underline mb-5">
+                        <span>Our Teachers</span>
+                    </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
+
+                    <div class="feature-1 border person text-center">
+                        <img src="images/person_1.jpg" alt="Image" class="img-fluid">
+                        <div class="feature-1-content">
+                            <h2>Craig Daniel</h2>
+                            <span class="position mb-3 d-block">Math Teacher</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
+                    <div class="feature-1 border person text-center">
+                        <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+                        <div class="feature-1-content">
+                            <h2>Taylor Simpson</h2>
+                            <span class="position mb-3 d-block">Math Teacher</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
+                    <div class="feature-1 border person text-center">
+                        <img src="images/person_3.jpg" alt="Image" class="img-fluid">
+                        <div class="feature-1-content">
+                            <h2>Jonas Tabble</h2>
+                            <span class="position mb-3 d-block">Math Teacher</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
+
+                    <div class="feature-1 border person text-center">
+                        <img src="images/person_4.jpg" alt="Image" class="img-fluid">
+                        <div class="feature-1-content">
+                            <h2>Craig Daniel</h2>
+                            <span class="position mb-3 d-block">Math Teacher</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
+                    <div class="feature-1 border person text-center">
+                        <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+                        <div class="feature-1-content">
+                            <h2>Taylor Simpson</h2>
+                            <span class="position mb-3 d-block">Math Teacher</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
+                    <div class="feature-1 border person text-center">
+                        <img src="images/person_3.jpg" alt="Image" class="img-fluid">
+                        <div class="feature-1-content">
+                            <h2>Jonas Tabble</h2>
+                            <span class="position mb-3 d-block">Math Teacher</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
