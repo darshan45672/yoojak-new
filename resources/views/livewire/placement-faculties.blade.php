b1c2b0e041484d22a1f77017d255eabb472eccde
darshan45672 <drshnbhandary@gmail.com>
2024-03-14 07:51:58 +0530

    added new migration and modified placement-faculties component

--- resources/views/livewire/placement-faculties.blade.php
+++ resources/views/livewire/placement-faculties.blade.php
@@ -5,7 +5,7 @@
             <div class="row mb-5 justify-content-center text-center">
                 <div class="col-lg-4 mb-5">
                     <h2 class="section-title-underline mb-5">
-                        <span>Our Teachers</span>
+                        <span>Our Faculties and Coordinators</span>
                     </h2>
                 </div>
             </div>
@@ -55,66 +55,54 @@
                 @endif
                 @endforeach
             </div>
-            <center>
-                <div class="row">
-                    @foreach ($faculties as $faculty)
-                    @if ($faculty->role == 4)
-                    <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
-                        <div class="feature-1 border person text-center">
-                            <img src="{{ asset('storage/' .$faculty->image) }}" alt="Image" class="img-fluid">
-                            <div class="feature-1-content">
-                                <h2>{{ $faculty->name }}</h2>
-                                <span class="position mb-3 d-block">{{ $faculty->designation }}</span>
-                                <p>{{ $faculty->role }}</p>
-                            </div>
-                        </div>
-                    </div>
-                    @endif
-                    @endforeach
-                </div>
-            </center>
+            <div class="row justify-content-center text-center">
+                @foreach ($faculties as $faculty)
+                @if ($faculty->role == 4)
                 <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
                     <div class="feature-1 border person text-center">
-                        <img src="images/person_3.jpg" alt="Image" class="img-fluid">
+                        <img src="{{ asset('storage/' .$faculty->image) }}" alt="Image" class="img-fluid">
                         <div class="feature-1-content">
-                            <h2>Jonas Tabble</h2>
-                            <span class="position mb-3 d-block">Math Teacher</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                            <h2>{{ $faculty->name }}</h2>
+                            <span class="position mb-3 d-block">{{ $faculty->designation }}</span>
+                            <p>{{ $faculty->role }}</p>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
-
-                    <div class="feature-1 border person text-center">
-                        <img src="images/person_4.jpg" alt="Image" class="img-fluid">
-                        <div class="feature-1-content">
-                            <h2>Craig Daniel</h2>
-                            <span class="position mb-3 d-block">Math Teacher</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
-                        </div>
-                    </div>
-                </div>
+                @endif
+                @endforeach
+            </div>
+            <div class="row justify-content-center text-center">
+                @foreach ($faculties as $faculty)
+                @if ($faculty->role == 5)
                 <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
                     <div class="feature-1 border person text-center">
-                        <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+                        <img src="{{ asset('storage/' .$faculty->image) }}" alt="Image" class="img-fluid">
                         <div class="feature-1-content">
-                            <h2>Taylor Simpson</h2>
-                            <span class="position mb-3 d-block">Math Teacher</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                            <h2>{{ $faculty->name }}</h2>
+                            <span class="position mb-3 d-block">{{ $faculty->designation }}</span>
+                            <p>{{ $faculty->role }}</p>
                         </div>
                     </div>
                 </div>
+                @endif
+                @endforeach
+            </div>
+            <div class="row justify-content-center text-center">
+                @foreach ($faculties as $faculty)
+                @if ($faculty->role == 6)
                 <div class="col-lg-4 col-md-6 mb-5 mb-lg-5">
                     <div class="feature-1 border person text-center">
-                        <img src="images/person_3.jpg" alt="Image" class="img-fluid">
+                        <img src="{{ asset('storage/' .$faculty->image) }}" alt="Image" class="img-fluid">
                         <div class="feature-1-content">
-                            <h2>Jonas Tabble</h2>
-                            <span class="position mb-3 d-block">Math Teacher</span>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi hendrerit elit</p>
+                            <h2>{{ $faculty->name }}</h2>
+                            <span class="position mb-3 d-block">{{ $faculty->designation }}</span>
+                            <p>{{ $faculty->role }}</p>
                         </div>
                     </div>
                 </div>
+                @endif
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
